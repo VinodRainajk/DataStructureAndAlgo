083e9e06bbd53ad62992ef661a1258682368d0b8
Vinod Raina <72406886+VinodRainajk@users.noreply.github.com>
2021-07-02 16:04:15 +0530

    New Examples

--- src/com/algoexpert/monotonic/Program.java
+++ src/com/algoexpert/monotonic/Program.java
@@ -1,5 +1,94 @@
 package com.algoexpert.monotonic;
 
-public class Program {
 
-}
+
+
+class Program {
+	  public static boolean isMonotonic(int[] array) {
+	    // Write your code here.
+		  
+		  boolean increasing = true;
+		  boolean equal = false;
+		  
+		  if(array.length<=1)
+		  {
+			  return true;
+		  }
+		  
+		  if(array[0]> array[1])
+		  {
+			  increasing = false;
+		  }else
+		  {
+			  if(array[0]< array[1])
+			  {
+				  increasing = true;  
+			  }else
+			  {
+				  increasing = false;
+				  equal = true;
+			  }
+			  
+		  }
+		  
+		  for(int idx = 1 ; idx< array.length ; idx++ )
+		  {
+			  if(equal )
+			  {
+				  if(array[idx-1] <  array[idx])
+				  {
+					  equal = false;
+					  increasing = true;  
+					  
+				  }else if(array[idx-1] >  array[idx])
+					  
+				  {
+					  equal = false;
+					  increasing = false;  
+				  }
+			  
+			  }
+		
+			  
+			  if(!(array[idx-1] == array[idx]))
+				{
+				
+					  if(!equal)
+					  {
+						  if(array[idx-1]==  array[idx]) 
+						  {
+							  equal = true; 
+						  }
+					  }
+					  
+					  if(!equal && increasing )
+					  {
+						  if(array[idx-1] >  array[idx])
+						  {
+							  return false;
+						  }
+						  
+					  }else if(!equal && (!increasing) )
+					  {
+						  
+						  if(array[idx-1] <  array[idx])
+						  {
+							  return false;
+						  }
+						  
+					  }
+				}
+			  
+			
+			  System.out.println("equal "+equal);
+			  System.out.println("increasing "+increasing);
+	
+		  }
+		  
+		  
+		  
+		  
+		  
+	    return true;
+	  }
+	}
---
+++ src/com/algoexpert/monotonic/Program2.java
@@ -0,0 +1,15 @@
+package com.algoexpert.monotonic;
+
+public class Program2 {
+	
+	public static void main(String[] args) {
+		
+		boolean first = true;
+		boolean second = true;
+		
+		System.out.println(first||second);
+		
+	}
+	
+
+}
